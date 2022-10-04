c554b019399ac6012289f234ddd99946109b32fb
msmithfl <msmithfl@icloud.com>
2022-10-04 19:56:03 -0400

    Restricts printing duplicate words

--- src/DadaIO.java
+++ src/DadaIO.java
@@ -17,13 +17,39 @@ public class DadaIO {
         int lines = scan.nextInt();
         System.out.println();
 
+        if (words.length < lines * 8) {
+            System.out.println("Not enough text. Sorry.");
+            System.exit(0);
+        }
+
+        printLines(words, lines);
+        
+        scan.close();
+    }
+
+    /*
+     * Name: printLines
+     * @param: words (String[])
+     * @param: lines (int)
+     * 
+     * Desc: prints out the words in a random order, restricted by the amount of lines
+     *          currently 8 words per line
+     * 
+     */
+    public static void printLines(String[] words, int lines) {
         for (int i = 0; i < lines; i++) {
+
             for (int j = 0; j < 8; j++) {
                 int randNum = (int)(Math.random() * words.length);
+
+                while (words[randNum] == null) {
+                    randNum = (int)(Math.random() * words.length);
+                }
+                
                 System.out.print(words[randNum] + " ");
+                words[randNum] = null;
             }
             System.out.println();
         }
-        scan.close();
     }
 }
